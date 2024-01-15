4919220688f26cc20d4f5bfa2e2e2b1b1ec9603f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-01-15 09:57:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/58-BCOn-drink-master',
+  base: '/58-BCOn-drink-master/',
   optimizeDeps: {
     // include: ['linked-dep'],
   },
